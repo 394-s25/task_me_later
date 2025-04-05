cd57abbb9a342e4ea73bcd0aafa2e97ce19086db
Isabella Fishman <fishman.bellam@gmail.com>
2025-04-05 15:50:42 -0500

    mapping but also not

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import TaskCardPreview from "./components/TaskCardPreview";
+import mockData from "../mock_data.json";
 
 const App = () => {
   return (
     <>
       <div>
         <h1 class="text-3xl font-bold underline">Hello world!!!!!!</h1>
-        <TaskCardPreview />
 
+        <TaskCardPreview taskData={mockData} />
       </div>
     </>
   );
--- src/components/TaskCardPreview.jsx
+++ src/components/TaskCardPreview.jsx
@@ -4,50 +4,53 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { Button } from "@mui/material";
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
 import { useState } from "react";
 import TaskCardModal from "./TaskCardModal";
 
-export default function TaskCardPreview() {
-  const [open,setOpen] = useState(false)
-  const [selectedCard, setSelectedCard] = useState(-1)
+export default function TaskCardPreview({ taskData }) {
+  const [open, setOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(-1);
   const handleCardClick = (taskId) => {
-    setOpen(true)
-    setSelectedCard(taskId)
-  }
-  const handleDialogClose = ()=> {
-    setOpen(false)
-    setSelectedCard(-1)
-  }
+    setOpen(true);
+    setSelectedCard(taskId);
+  };
+  const handleDialogClose = () => {
+    setOpen(false);
+    setSelectedCard(-1);
+  };
   return (
     <>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea onClick={() => {handleCardClick}}>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            task_title
-          </Typography>
-          <Typography>
-            due_date
-          </Typography>
-          <Typography variant="body1">
-            task_dependency_list
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            task_score
-            <br />
-            task_match
-          </Typography>
-          <Typography>
-            <Button href="/add_task">
-              Add Task
-            </Button>
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    < TaskCardModal taskId ={selectedCard} open={open} onClose={handleDialogClose} />
-    </ >
+      {taskData.task_card_preview_mock.map((taskItem) => {
+        <>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea
+              onClick={() => {
+                handleCardClick;
+              }}
+            >
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {taskItem.task_title}
+                </Typography>
+                <Typography>due_date</Typography>
+                <Typography variant="body1">task_dependency_list</Typography>
+                <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                  {taskItem.task_score}
+                  <br />
+                  {taskItem.task_match}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <TaskCardModal
+            taskId={selectedCard}
+            open={open}
+            onClose={handleDialogClose}
+          />
+        </>;
+      })}
+    </>
   );
 }
