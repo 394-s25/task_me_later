5a1f7e763e107e8b5aa2842830b2c931783e8a3e
MusicTastingClubNU <musictastingclub@gmail.com>
2025-04-06 16:59:33 -0500

    Added buttons to tasks on home page

--- src/components/TaskCardPreview.jsx
+++ src/components/TaskCardPreview.jsx
@@ -1,18 +1,13 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { Button } from "@mui/material";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
 import { useState } from "react";
 import TaskCardModal from "./TaskCardModal";
 import taskData2 from "../../mock_data.json";
-import CloseIcon from "@mui/icons-material/Close";
 import tml_logo from "../imgs/tml_logo.png";
 import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
 
 export default function TaskCardPreview() {
   const [open, setOpen] = useState(false);
@@ -25,10 +20,14 @@ export default function TaskCardPreview() {
     setOpen(false);
     setSelectedCard(-1);
   };
+
   return (
     <>
-      <img className="p-3 mx-auto items-center" src={tml_logo}></img>
-      <div className="bg-gray-100 rounded-2xl">
+      <img
+        className="p-3 mx-auto items-center min-h-[100%]"
+        src={tml_logo}
+      ></img>
+      <div className="bg-gray-100 rounded-2xl ">
         <div className="flex flex-wrap justify-center items-center mx-auto bg-gray">
           {taskData2.tasks.map((taskItem) => (
             <>
@@ -41,21 +40,75 @@ export default function TaskCardPreview() {
               >
                 <CardActionArea>
                   <CardContent>
-                    <h1>{taskItem.task_title}</h1>
+                    <h1 className="font-bold">{taskItem.task_title}</h1>
                     <h1 className="text-[12px]">Due: {taskItem.due_date}</h1>
                     <hr />
-                    <div>
-                      {/* <Stack direction="row" spacing={1}> */}
-                      <Chip
-                        label="primary"
-                        color="primary"
-                        size="small"
-                        className="w-100"
-                      />
-                      <Chip label="success" color="success" size="small" />
+                    <div className="mt-1">
+                      {taskItem.task_status === "Completed" ? (
+                        <Chip
+                          label={taskItem.task_status}
+                          size="small"
+                          style={{
+                            backgroundColor: "lightgreen",
+                            color: "black",
+                          }}
+                        />
+                      ) : taskItem.task_status === "In Progress" ? (
+                        <Chip
+                          label={taskItem.task_status}
+                          size="small"
+                          style={{
+                            backgroundColor: "orange",
+                            color: "black",
+                          }}
+                        />
+                      ) : (
+                        taskItem.task_status === "To Do" && (
+                          <Chip
+                            label={taskItem.task_status}
+                            size="small"
+                            style={{
+                              backgroundColor: "red",
+                              color: "white",
+                            }}
+                          />
+                        )
+                      )}
+
+                      {/* <Chip label="success" color="success" size="small" /> */}
                       {/* </Stack> */}
                     </div>
-
+                    <hr className="mt-1" />
+                    <div className="flex flex-col items-center mx-auto">
+                      <Button
+                        style={{
+                          backgroundColor: "yellow",
+                          color: "black",
+                          marginLeft: 1,
+                          marginTop: 5,
+                          height: 20,
+                          border: "1px solid lightgray",
+                        }}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
+                      >
+                        GET HELP
+                      </Button>
+                      <Button
+                        style={{
+                          backgroundColor: "lightgreen",
+                          color: "black",
+                          marginTop: 5,
+                          height: 20,
+                        }}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                        }}
+                      >
+                        MARK DONE
+                      </Button>
+                    </div>
                     {/* <Typography
                       variant="body2"
                       sx={{ color: "text.secondary" }}
