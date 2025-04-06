3fb422039c6a50e9e665f503c12f0793d8cb8ca8
MusicTastingClubNU <musictastingclub@gmail.com>
2025-04-06 14:38:05 -0500

    Profile Page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,25 +3,20 @@ import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import RestoreIcon from "@mui/icons-material/Restore";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ArchiveIcon from "@mui/icons-material/Archive";
 import Paper from "@mui/material/Paper";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
-import ListItemText from "@mui/material/ListItemText";
-import Avatar from "@mui/material/Avatar";
 import HomeIcon from "@mui/icons-material/Home";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import EmailIcon from "@mui/icons-material/Email";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { useNavigate } from "react-router-dom";
+import Profile from "../pages/Profile";
 
 export default function NavBar() {
   const [value, setValue] = React.useState(0);
-  const navigate = useNavigate;
-
+  const navigate = useNavigate();
+  const goToProfile = () => {
+    navigate("/Profile");
+  };
   return (
     <Box sx={{ pb: 7 }}>
       <CssBaseline />
@@ -39,9 +34,11 @@ export default function NavBar() {
           <BottomNavigationAction label="Home" icon={<HomeIcon />} />
           <BottomNavigationAction label="Projects" icon={<AssignmentIcon />} />
           <BottomNavigationAction label="Messages" icon={<EmailIcon />} />
-          <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />}>
-            <button>hello world</button>
-          </BottomNavigationAction>
+          <BottomNavigationAction
+            label="Profile"
+            icon={<AccountCircleIcon />}
+            onClick={goToProfile}
+          />
         </BottomNavigation>
       </Paper>
     </Box>
