bf52da202cb827adbe49b785b41ab9ac4e4be9cf
Luke Huff <lukerhuff@gmail.com>
2025-04-21 14:10:20 -0500

    messages functionality init commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import SignUpLoginPage from "./pages/SignUp";
 import SignUp from "./pages/CreateAccount";
 import { useAuthContext } from "./services/userProvider";
 import { Navigate } from "react-router-dom";
+import ChatWindow from "./components/ChatWindow";
+import NewConversation from "./pages/NewConversation";
 
 const App = () => {
   const PrivateRoute = ({ children }) => {
@@ -40,6 +42,22 @@ const App = () => {
           element={
             <PrivateRoute>
               <Messages />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/chat/:conversationId"
+          element={
+            <PrivateRoute>
+              <ChatWindow />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/new-conversation"
+          element={
+            <PrivateRoute>
+              <NewConversation />
             </PrivateRoute>
           }
         />
--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,11 +1,40 @@
+import React from "react";
 import NavBar from "../components/NavBar";
+import ConversationList from "../components/ConversationList";
+import { Button, Box, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import AddCommentIcon from "@mui/icons-material/AddComment";
 
 const Messages = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1>Messages Tab</h1>
+      <Box sx={{ p: 3, pb: 7 }}>
+        {" "}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            mb: 2,
+          }}
+        >
+          <Typography variant="h5">Messages</Typography>
+          <Button
+            startIcon={<AddCommentIcon />}
+            variant="contained"
+            onClick={() => navigate("/new-conversation")}
+          >
+            New Conversation
+          </Button>
+        </Box>
+        <ConversationList />
+      </Box>
+
       <NavBar />
     </>
   );
 };
+
 export default Messages;
